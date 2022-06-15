4a60086f0badd9b5041039e3a58984b6ef52d052
Vicente <vicente.costa@ua.pt>
2022-06-15 18:52:47 +0100

    clean up test

--- demo/src/test/java/com/example/demo/login/LoginSteps.java
+++ demo/src/test/java/com/example/demo/login/LoginSteps.java
@@ -49,103 +49,97 @@ public class LoginSteps {
 		driver.get(baseUrl);
     }
 
-    @Given("I am on the {string} page")
-    public void go_to_page(final String url) {
-        System.out.println(baseUrl+url);
-        System.out.println(driver.getCurrentUrl());
-		assertEquals(baseUrl+url, driver.getCurrentUrl());
-    }
     @When("loggin with the credencials {string} and {string}")
     public void loggin_with_credencials(final String email, final String password) {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("I wait "+int1+" hour");
-        driver.findElement(By.name("email")).click();
-		driver.findElement(By.name("email")).clear();
+
 		driver.findElement(By.name("email")).sendKeys("costav689@gmail.com");
-
-		driver.findElement(By.name("password")).click();
-		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys("asdfqwer");
-
-        driver.findElement(By.xpath("//body")).click();
 		driver.findElement(By.xpath("//div[@id='root']/div/form/div/div/div[3]/button")).click();
-
-    }
-
-
-    @When("i accept the delivery with the name {string}")
-    public void accept_job(final String delivery) {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("I wait "+int1+" hour");
-
-		driver.findElement(By.xpath("//*[contains(text(),'"+delivery+"')]/div/a/button")).click();
-
-    }
-
-
-    @When("i click on details of the delivery with the name {string}")
-    public void details_product(final String delivery) {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("I wait "+int1+" hour");
-
-        driver.findElement(By.xpath("//*[contains(text(),'"+delivery+"')]/div/a[2]/button")).click();
-
-    }
-
-    @When("I accompish the job")
-    public void job_done() {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("I wait "+int1+" hour");
-
-		driver.findElement(By.xpath("//div[@id='root']/div/main/div/button")).click();
-
-    }
-
-    @When("I click on logout")
-    public void log_out() {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("I wait "+int1+" hour");
-
-		driver.findElement(By.linkText("Logout")).click();
 
     }
 
     @Then("I expect to be on the {string} page")
     public void assert_page_url_end(final String route) {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("my belly should growl");
 
 		assertEquals(baseUrl+route, driver.getCurrentUrl());
         driver.quit();
 
     }
 
+
+
+
+
+    @When("i accept the delivery with the name {string}")
+    public void accept_job(final String delivery) {
+
+		driver.findElement(By.xpath("//*[contains(text(),'"+delivery+"')]/div/a/button")).click();
+
+    }
 
     @Then("I expect to be on the page {string}")
     public void assert_page_url(final String route) {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("my belly should growl");
 
 		assertEquals(baseUrl+route, driver.getCurrentUrl());
 
     }
-        
+
     @And("see the product name {string}")
     public void assert_product_name_quit(final String name) {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("my belly should growl");
+
         String product_name = driver.findElement(By.xpath("//div[@id='root']/div/main/div/div/h1")).getText();
 		assertEquals(name.equals(product_name), true);
         driver.quit();
 
     }
+
+
+
+
+    @When("i click on details of the delivery with the name {string}")
+    public void details_product(final String delivery) {
+
+        driver.findElement(By.xpath("//*[contains(text(),'"+delivery+"')]/div/a[2]/button")).click();
+
+    }
+
+
+
+
+    @When("I accompish the job")
+    public void job_done() {
+
+		driver.findElement(By.xpath("//div[@id='root']/div/main/div/button")).click();
+
+    }
+
     @And("see the product named {string}")
     public void assert_product_name(final String name) {
-        // Write code here that turns the phrase above into concrete actions
-        //System.out.println("my belly should growl");
+
         String product_name = driver.findElement(By.xpath("//div[@id='root']/div/main/div/div/h1")).getText();
 		assertEquals(name.equals(product_name), true);
 
     }
+
+
+
+
+    @When("I click on logout")
+    public void log_out() {
+
+		driver.findElement(By.linkText("Logout")).click();
+
+    }
+
+
+
+
+
+    // @Given("I am on the {string} page")
+    // public void go_to_page(final String url) {
+    //     System.out.println(baseUrl+url);
+    //     System.out.println(driver.getCurrentUrl());
+	// 	assertEquals(baseUrl+url, driver.getCurrentUrl());
+    // }
 
 }
